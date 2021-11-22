0a02e91d78f900d04d2a51a2c2577743473da84b
Sofiyaparvinshaik <sofiyaparvinshaik@gmail.com>
2021-11-22 22:55:00 +0530

    Update PromotionalEngineServiceImpl.java

--- PromotionEngine/src/com/promotionalEngine/service/PromotionalEngineServiceImpl.java
+++ PromotionEngine/src/com/promotionalEngine/service/PromotionalEngineServiceImpl.java
@@ -1,5 +1,45 @@
 package com.promotionalEngine.service;
 
-public class PromotionalEngineServiceImpl {
+import com.promotionalEngine.PromotionalEngine;
+import com.promotionalEngine.Entities.Items;
+import com.promotionalEngine.Entities.SKU;
+
+public class PromotionalEngineServiceImpl implements IPromotionalEngine{
+
+	@Override
+	public boolean addNewSKUIDsRepository(SKU skuEntity) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addPromotionalDetailsRepository(PromotionalEngine activePromotions) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updatePrmotionalDetailsRepository(PromotionalEngine activePromotions) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeSKUIDs(String SKUID) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Integer getCartTotal() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addItemToCart(Items Item) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
